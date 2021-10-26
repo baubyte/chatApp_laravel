8cdf4e81c926e9a8598e10ec3a1af874e4633658
baubyte <paredbaez.martin@gmail.com>
2021-10-25 23:46:54 -0300

    realciones

--- app-src/app/Models/Chat.php
+++ app-src/app/Models/Chat.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Chat extends Model
 {
     use HasFactory;
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
