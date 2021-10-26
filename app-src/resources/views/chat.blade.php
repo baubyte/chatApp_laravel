77d24a13ecb5b14a169fb3ddbbb0d8622762c7bf
baubyte <paredbaez.martin@gmail.com>
2021-10-26 20:36:04 -0300

    tittle change and assets

--- app-src/resources/views/chat.blade.php
+++ app-src/resources/views/chat.blade.php
@@ -2,13 +2,13 @@
 <html lang="en" >
 	<head>
 		<meta charset="UTF-8">
-		<title>CodePen - Simple Chat UI</title>
+		<title>Simple Chat BAUBYTE</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
-		<link rel="stylesheet" href="/css/app.css">
-		<link rel="stylesheet" href="/css/chat.css">
+		<link rel="stylesheet" href="{{asset('css/app.css')}}">
+		<link rel="stylesheet" href="{{asset('css/chat.css')}}">
 	</head>
 	<body>
 		<section class="msger">
@@ -31,7 +31,7 @@
 			</form>
 		</section>
 		<script src='https://use.fontawesome.com/releases/v5.0.13/js/all.js'></script>
-		<script  src="/js/app.js"></script>
-		<script  src="/js/chat.js"></script>
+		<script  src="{{asset('js/app.js')}}"></script>
+		<script  src="{{asset('js/chat.js')}}"></script>
 	</body>
 </html>
