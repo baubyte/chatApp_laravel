eece3fafadbe1b5498f9f48360e9cf737e18166f
baubyte <paredbaez.martin@gmail.com>
2021-11-10 01:42:04 -0300

    mostrar chats

--- app-src/routes/web.php
+++ app-src/routes/web.php
@@ -13,13 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->middleware(['auth'])->middleware(['auth'])->name('dashboard');;
 
 require __DIR__.'/auth.php';
 
